2eae0e66fccda8fe578c1f689b1bad16a12690c8
Verun Rahimtoola <vrahimtoola@eharmony.com>
2013-12-18 17:14:20 -0800

    moving some tests around

---
+++ src/test/java/com/eharmony/matching/vw/webservice/util/tests/StringIterableTest.java
@@ -0,0 +1,46 @@
+/**
+ * 
+ */
+package com.eharmony.matching.vw.webservice.util.tests;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+import com.eharmony.matching.vw.webservice.util.StringIterable;
+
+/**
+ * @author vrahimtoola Tests the StringIterable.
+ */
+public class StringIterableTest {
+
+	@Test(expected = IllegalArgumentException.class)
+	public void throwsExceptionOnNullChunkOfTextTest() {
+		new StringIterable(null);
+	}
+
+	@Test
+	public void BasicNewLinesTest() {
+		String theTextString = "Line 1\nLine 2\nLine 3";
+
+		StringIterable stringIterable = new StringIterable(theTextString);
+
+		int x = 0;
+		for (String ln : stringIterable) {
+			switch (x++) {
+			case 0:
+				Assert.assertEquals("Line 1", ln);
+				break;
+			case 1:
+				Assert.assertEquals("Line 2", ln);
+				break;
+			case 2:
+				Assert.assertEquals("Line 3", ln);
+				break;
+			default:
+				Assert.fail();
+			}
+
+		}
+	}
+}
