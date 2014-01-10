7024d0f743f53ad723d7dd38e8bd0c999d772526
Verun Rahimtoola <vrahimtoola@eharmony.com>
2014-01-10 15:14:32 -0800

    added a comment

--- src/test/java/com/eharmony/matching/vw/webservice/client/JerseyClientTest.java
+++ src/test/java/com/eharmony/matching/vw/webservice/client/JerseyClientTest.java
@@ -22,6 +22,7 @@ import org.glassfish.jersey.client.ClientConfig;
 import org.glassfish.jersey.client.ClientProperties;
 import org.glassfish.jersey.client.RequestEntityProcessing;
 import org.glassfish.jersey.grizzly.connector.GrizzlyConnectorProvider;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -29,14 +30,13 @@ import org.slf4j.LoggerFactory;
 import com.google.common.base.Charsets;
 
 /**
- * @author vrahimtoola
- * 
+ * @author vrahimtoola This is the only client that actually works!
  */
 public class JerseyClientTest {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(JerseyClientTest.class);
 
-	//@Ignore
+	@Ignore
 	@Test
 	public void test() throws IOException, InterruptedException, ExecutionException {
 
