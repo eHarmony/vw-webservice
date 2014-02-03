01433c787e1f1a193e77edd1e40ff7bd26c2b4b0
Verun Rahimtoola <vrahimtoola@eharmony.com>
2014-02-03 11:52:58 -0800

    moving example and prediction media types to common project

---
+++ vw-webservice-common/src/main/java/com/eharmony/matching/vw/webservice/common/prediction/PredictionMediaTypes.java
@@ -0,0 +1,15 @@
+package com.eharmony.matching.vw.webservice.common.prediction;
+
+/**
+* @author vrahimtoola
+*
+* String constants for prediction media types (when sending back vw
+* predictions).
+*/
+public class PredictionMediaTypes {
+
+	public static final String PLAINTEXT_1_0 = "application/vowpal-wabbit-prediction-v1.0+plaintext";
+
+}
+
+
